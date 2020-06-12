0c04f8adb31fc5612613a16d0788003ec3de9f48
rmr1154 <38836662+rmr1154@users.noreply.github.com>
2020-06-12 09:22:00 -0400

    restructuring

---
+++ nucampsite/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Main from './components/MainComponent';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
+
+console.disableYellowBox = true;
+
+const store = ConfigureStore();
+
+export default function App() {
+  return (
+    <Provider store={store}>
+      <Main />
+    </Provider>
+  );
+};
+
---
+++ nucampsite/components/AboutComponent.js
@@ -0,0 +1,90 @@
+import React, { Component } from 'react';
+import { Card, ListItem } from 'react-native-elements';
+import { ScrollView, Text, FlatList } from 'react-native';
+import { connect } from 'react-redux';
+import { baseUrl } from '../shared/baseUrl';
+import Loading from './LoadingComponent';
+
+const mapStateToProps = state => {
+    return {
+        partners: state.partners
+    };
+};
+
+function Mission() {
+
+    return (
+        <Card
+            title='Our Mission'>
+            <Text>We present a curated database of the best
+            campsites in the vast woods and backcountry of
+            the World Wide Web Wilderness. We increase access to adventure for
+            the public while promoting safe and respectful use of
+            resources. The expert wilderness trekkers on our staff
+            personally verify each campsite to make sure that they
+            are up to our standards. We also present a platform for
+            campers to share reviews on campsites they have visited
+            with each other.
+                        </Text>
+        </Card>
+    );
+
+    return <View />;
+};
+
+class AboutUs extends Component {
+
+    static navigationOptions = {
+        title: 'About Us'
+    };
+
+    render() {
+        const { navigate } = this.props.navigation;
+
+        const renderPartner = ({ item }) => {
+            return (
+                <ListItem
+                    title={item.name}
+                    subtitle={item.description}
+                    onPress={() => navigate('CampsiteInfo', { campsiteId: item.id })}
+                    leftAvatar={{source: {uri: baseUrl + item.image}}}
+                />
+            );
+        };
+        if (this.props.partners.isLoading) {
+            return (
+                <ScrollView>
+                    <Mission />
+                    <Card
+                        title='Community Partners'>
+                        <Loading />
+                    </Card>
+                </ScrollView>
+            );
+        }
+        if (this.props.partners.errMess) {
+            return (
+                <ScrollView>
+                    <Mission />
+                    <Card
+                        title='Community Partners'>
+                        <Text>{this.props.partners.errMess}</Text>
+                    </Card>
+                </ScrollView>
+            );
+        }
+        return (
+            <ScrollView>
+                <Mission />
+                <FlatList
+                    data={this.props.partners.partners}
+                    renderItem={renderPartner}
+                    keyExtractor={item => item.id.toString()}
+                />
+            </ScrollView>
+        );
+    };
+}
+
+ 
+export default connect(mapStateToProps)(AboutUs);
